9f0d18aac184660d29e0674fb00d87d4106e1e78
Kraeks <kraeks@astra2441.startdedicated.de>
2020-03-21 15:36:56 +0100

    content-Types for Landing-Page

--- src/nva/frontpage/content/card.py
+++ src/nva/frontpage/content/card.py
@@ -1,60 +1,26 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
 from plone.dexterity.content import Item
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
-
-
-# from nva.frontpage import _
+from plone.namedfile.field import NamedBlobImage
+from z3c.relationfield.schema import RelationChoice
+from plone.app.vocabularies.catalog import CatalogSource
 
 
 class ICard(model.Schema):
     """ Marker interface and Dexterity Python Schema for Card
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('card.xml')
+    cardclass = schema.TextLine(title=u"Style-Klasse der Karte", required=False)
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    cardhaeder = schema.TextLine(title=u"Kopf der Karte", required=False)
+    
+    calltoaction = RelationChoice(title=u"Referenz auf der Seite", source=CatalogSource(), required=False)
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    buttonclass = schema.TextLine(title=u"Buttonklasse", required=False)
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    cardimage = NamedBlobImage(title=u"Titelbild der Karte", required=False)
 
 
 @implementer(ICard)
--- src/nva/frontpage/content/landingpage.py
+++ src/nva/frontpage/content/landingpage.py
@@ -1,60 +1,20 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.app.textfield import RichText
 from plone.dexterity.content import Container
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
-
-
-# from nva.frontpage import _
 
 
 class ILandingpage(model.Schema):
     """ Marker interface and Dexterity Python Schema for Landingpage
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('landingpage.xml')
+    text = RichText(title=u"Haupttext der Seite", required=False)
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    show_title = schema.Bool(title=u"Titel der Seite anzeigen", default=False)
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    show_description = schema.Bool(title=u"Kurzbeschreibung der Seite anzeigen", default=False)
 
 
 @implementer(ILandingpage)
