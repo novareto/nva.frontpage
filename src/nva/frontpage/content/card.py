f1954a1bb52dab6fc811b0d6d44eed01842ad2ed
Kraeks <kraeks@astra2441.startdedicated.de>
2020-03-23 11:43:17 +0100

    Bugfix LandingPage

--- src/nva/frontpage/content/card.py
+++ src/nva/frontpage/content/card.py
@@ -12,13 +12,17 @@ class ICard(model.Schema):
     """ Marker interface and Dexterity Python Schema for Card
     """
 
-    cardclass = schema.TextLine(title=u"Style-Klasse der Karte", required=False)
+    cardclass = schema.TextLine(title=u"Style-Klasse der Karte", required=False, default=u"card col-12")
 
-    cardhaeder = schema.TextLine(title=u"Kopf der Karte", required=False)
+    cardbodyclass = schema.TextLine(title=u"Body-Klasse der Karte", required=False, default=u"card-body")
+
+    cardheader = schema.TextLine(title=u"Kopf der Karte", required=False)
     
     calltoaction = RelationChoice(title=u"Referenz auf der Seite", source=CatalogSource(), required=False)
 
-    buttonclass = schema.TextLine(title=u"Buttonklasse", required=False)
+    buttontext = schema.TextLine(title=u"Beschriftung des Buttons", required=False, default="Lerne mehr")
+
+    buttonclass = schema.TextLine(title=u"Buttonklasse", required=False, default=u"btn btn-secondary")
 
     cardimage = NamedBlobImage(title=u"Titelbild der Karte", required=False)
 
--- src/nva/frontpage/views/landing_view.py
+++ src/nva/frontpage/views/landing_view.py
@@ -2,6 +2,7 @@
 
 from nva.frontpage import _
 from Products.Five.browser import BrowserView
+from plone import api as ploneapi
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -13,5 +14,17 @@ class LandingView(BrowserView):
 
     def __call__(self):
         # Implement your own actions:
-        self.msg = _(u'A small message')
+        self.pagecontents = self.get_pagecontents()
         return self.index()
+
+    def get_pagecontents(self):
+        return self.context.listFolderContents()
+
+    def getRemote(self, item):
+        remote = item.remoteUrl
+        if 'resolveuid' in remote:
+            uid = remote.split('/')[-1]
+            obj = ploneapi.content.get(UID=uid)
+            if obj:
+                return obj.absolute_url()
+        return remote
