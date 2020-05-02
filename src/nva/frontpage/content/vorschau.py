a5d1518320f340da5997c7f1054a3f47e15b426c
Lars Walther <meinkraeks@gmail.com>
2020-05-02 12:10:03 +0200

    jfjldfjs

--- src/nva/frontpage/content/vorschau.py
+++ src/nva/frontpage/content/vorschau.py
@@ -1,60 +1,52 @@
 # -*- coding: utf-8 -*-
 # from plone.app.textfield import RichText
-# from plone.autoform import directives
 from plone.dexterity.content import Item
-# from plone.namedfile import field as namedfile
+from plone.app.vocabularies.catalog import CatalogSource
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+from plone.namedfile.field import NamedBlobImage
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
+from zope.interface import Invalid
+from zope.interface import invariant
 
 
-# from nva.frontpage import _
+from nva.frontpage import _
+
+
+directions = SimpleVocabulary((
+                 SimpleTerm(value=u'left', token=u'left', title=u'links'),
+                 SimpleTerm(value=u'center', token=u'center', title=u'mittig'),
+                 SimpleTerm(value=u'right', token=u'right', title=u'rechts'),
+                 ))
 
 
 class IVorschau(model.Schema):
     """ Marker interface and Dexterity Python Schema for Vorschau
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('vorschau.xml')
+    dachzeile = schema.TextLine(title=u"Dachzeile", required=False)
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    link = schema.URI(title=u"Link auf eine externe Information", required=False)
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    verweis = schema.RelationChoice(title=u"Interner Verweis", required=False,
+                                    description=u"Wenn vorhanden überschreibt der Verweis den externen Link.",
+                                    source=CatalogSource())
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    linktitle = schema.TextLine(title=u"Titel des Links auf der Landingpage", required=False)
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
+    image = NamedBlobImage(title=u"Vorschaubild", required=False)
 
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
+    ausrichtung = schema.Choice(title=u"Ausrichtung der Ansicht", required=True,
+                                    vocabulary=directions,
+                                    default='left')
 
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    @invariant
+    def link_invariant(data):
+        if data.link or data.verweis:
+            if not data.linktitle:
+                raise Invalid(_(u'Bei Auswahl eines Links muss ein Linktitel angegeben werden.'))
 
 
 @implementer(IVorschau)
