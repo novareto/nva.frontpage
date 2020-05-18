7f841136e6b756db49dfc40475765b25c47325bc
mduennwald <duennwald.miriam@bgetem.de>
2020-05-18 16:16:10 +0200

    added views for vorschau

--- src/nva/frontpage/views/landing_view.py
+++ src/nva/frontpage/views/landing_view.py
@@ -3,6 +3,8 @@
 from nva.frontpage import _
 from Products.Five.browser import BrowserView
 from plone import api as ploneapi
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
 
 class LandingView(BrowserView):
 
@@ -10,12 +12,18 @@ class LandingView(BrowserView):
         self.pagecontents = self.get_pagecontents()
         return self.index()
 
+    def getMacro(self, pageitem):
+        vorschauname = ViewPageTemplateFile('templates/vorschau/vorschau_'+pageitem.blockshape+'_view.pt').id.split("_")[1]
+        return ViewPageTemplateFile('templates/vorschau/vorschau_' + vorschauname + '_view.pt').macros[vorschauname]
+
     def createSnippet(self, obj):
         if obj.portal_type == 'Link':
             return createLinkSnippet(obj)
 
     def get_pagecontents(self):
         objekte = self.context.listFolderContents()
+        for item in objekte:
+            item.url = item.absolute_url()
         return objekte
 
     def getRemote(self, item):
@@ -34,20 +42,20 @@ class LandingView(BrowserView):
         elif item.start and not item.end:
             event = "%s" % item.start.strftime('%d.%m.%Y')
         if item.location:
-            event += ", %s" %item.location
+            event += ", %s" % item.location
         return event
-            
+
     def getCollectionEntries(self, item):
-        data = {'cards':[], 'morelink':None}
+        data = {'cards': [], 'morelink': None}
         artikel = ploneapi.content.get_view('enhanced_foldersummary', item, self.request).contentlist()
         if item.deckshape == '1-4-0':
             if len(artikel) > 4:
                 artikel = artikel[:4]
-                data['morelink'] = item.decklink #Der Link mit weiteren Objekten muss angezeigt werden
+                data['morelink'] = item.decklink  # Der Link mit weiteren Objekten muss angezeigt werden
             data['cards'] = artikel
         elif item.deckshape == '2-3-4':
             if len(artikel) > 7:
                 artikel = artikel[:7]
-                data['morelink'] = item.decklink #Der Link mit weiteren Objekten muss angezeigt werden
+                data['morelink'] = item.decklink  # Der Link mit weiteren Objekten muss angezeigt werden
             data['cards'] = [artikel[:3], artikel[3:]]
         return data
---
+++ src/nva/frontpage/views/vorschau_view.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+from nva.frontpage import _
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class VorschauStageView(BrowserView):
+    template = ViewPageTemplateFile('templates/vorschau/vorschau_stage_view.pt')
+
+    def __call__(self):
+        self.vorschau = self.context
+        if self.context.verweis:
+            self.vorschau.verweis = self.context.verweis.absolute_url()
+        else:
+            self.vorschau.verweis = self.context.link
+        self.vorschau = self.context.__dict__
+        return self.template()
+
+
+class VorschauTextblockView(BrowserView):
+    template = ViewPageTemplateFile('templates/vorschau/vorschau_textblock_view.pt')
+
+    def __call__(self):
+        self.vorschau = self.context
+        if self.context.verweis:
+            self.vorschau.verweis = self.context.verweis.absolute_url()
+        else:
+            self.vorschau.verweis = self.context.link
+        self.vorschau = self.context.__dict__
+        return self.template()
+
+
+class VorschauLeftBottomView(BrowserView):
+    template = ViewPageTemplateFile('templates/vorschau/vorschau_left-bottom_view.pt')
+
+    def __call__(self):
+        self.vorschau = self.context
+        if self.context.verweis:
+            self.vorschau.verweis = self.context.verweis.absolute_url()
+        else:
+            self.vorschau.verweis = self.context.link
+        self.vorschau = self.context.__dict__
+        return self.template()
+
+
+class VorschauLeftTopView(BrowserView):
+    template = ViewPageTemplateFile('templates/vorschau/vorschau_left-top_view.pt')
+
+    def __call__(self):
+        self.vorschau = self.context
+        if self.context.verweis:
+            self.vorschau.verweis = self.context.verweis.absolute_url()
+        else:
+            self.vorschau.verweis = self.context.link
+        self.vorschau = self.context.__dict__
+        return self.template()
+
+
+class VorschauRightBottomView(BrowserView):
+    template = ViewPageTemplateFile('templates/vorschau/vorschau_right-bottom_view.pt')
+
+    def __call__(self):
+        self.vorschau = self.context
+        if self.context.verweis:
+            self.vorschau.verweis = self.context.verweis.absolute_url()
+        else:
+            self.vorschau.verweis = self.context.link
+        self.vorschau = self.context.__dict__
+        return self.template()
+
+
+class VorschauRightTopView(BrowserView):
+    template = ViewPageTemplateFile('templates/vorschau/vorschau_right-top_view.pt')
+
+    def __call__(self):
+        self.vorschau = self.context
+        if self.context.verweis:
+            self.vorschau.verweis = self.context.verweis.absolute_url()
+        else:
+            self.vorschau.verweis = self.context.link
+        self.vorschau = self.context.__dict__
+        return self.template()
